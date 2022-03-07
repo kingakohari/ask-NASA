5f8c9bb372f76e512ada23ab9a7a431d9a4bb132
Kinga Kohari <kinguca@yahoo.com>
2022-03-07 13:02:20 +0100

    API fetch completed for today's date

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,42 @@
-document.querySelector('form').addEventListener('submit', nasa)
 
-function nasa(e){
-  e.preventDefault()
-  let date = document.querySelector('input').value
-  console.log(date)
-  fetch(`https://api.nasa.gov/planetary/apod?date=${date}&hd=true&api_key=PX6KQodjwM0QXhS5JSHPYRAp7WyPGe8J2ui06hFi`)
-    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-    .then(response => {
-        console.log(response)
-        document.querySelector('h2').innerText=response.title;
-        if(response.media_type == "image"){
-          document.querySelector('img').src = response.hdurl
-        }else{
-          let iframe=document.querySelector('iframe');
-          iframe.src = response.url
-          iframe.style.display="block"
-        }
-        document.getElementById('description').innerText= response.explanation;
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-        alert("sorry, there are no results for your search")
-    });
+function loadEvent() {
+
+  const rootElement = document.getElementById("root")
+
+  const form = rootElement.querySelector("form")
+
+  form.addEventListener("submit",function(event) {
+    event.preventDefault()
+  });
+  
+  let requestedDate = document.querySelector('input').value
+
+  const apiKey = "bT4tpRHIuLBqvqfsmafjX2qeyIaykEQUv26jx1wg"
+
+
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${requestedDate}`).then(
+    function (apodResponse){
+        console.log(apodResponse);
+        apodResponse.json().then(
+            function (apodResponseJson) {
+              
+            document.querySelector("h2").innerText = apodResponseJson.title
+
+            if (apodResponseJson.media_type === "image"){
+
+              document.querySelector("img").src = apodResponseJson.hdurl
+
+              } else {
+
+              let iframe=document.querySelector("iframe");
+              iframe.src = apodResponseJson.url
+              iframe.style.display="block"
+            }
+
+            document.getElementById("description").innerText= apodResponseJson.explanation;
+          });
+    }
+  )
 }
+
+window.addEventListener("load", loadEvent) 
