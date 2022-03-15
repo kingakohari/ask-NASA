00f46a40241ba6508ec1607096e3e0279b84e8de
Kinga Kohari <kinguca@yahoo.com>
2022-03-15 01:05:49 +0100

    Fixed the date picker

--- frontend/script.js
+++ frontend/script.js
@@ -1,42 +1,49 @@
 
 function loadEvent() {
 
+
   const rootElement = document.getElementById("root")
 
   const form = rootElement.querySelector("form")
 
-  form.addEventListener("submit",function(event) {
-    event.preventDefault()
-  });
+  form.addEventListener("submit", function(e) {
   
-  let requestedDate = document.querySelector('input').value
+    e.preventDefault()
 
-  const apiKey = "bT4tpRHIuLBqvqfsmafjX2qeyIaykEQUv26jx1wg"
+    let requestedDate = document.getElementById("date").value
 
+    console.log(requestedDate);
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${requestedDate}`).then(
-    function (apodResponse){
-        console.log(apodResponse);
-        apodResponse.json().then(
-            function (apodResponseJson) {
-              
-            document.querySelector("h2").innerText = apodResponseJson.title
+    const apiKey = "bT4tpRHIuLBqvqfsmafjX2qeyIaykEQUv26jx1wg"
 
-            if (apodResponseJson.media_type === "image"){
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${requestedDate}`).then(
+      function (apodResponse){
+          console.log(apodResponse);
+          apodResponse.json().then(
+              function (apodResponseJson) {
+                
+              document.querySelector("h2").innerText = apodResponseJson.title
 
-              document.querySelector("img").src = apodResponseJson.hdurl
+              if (apodResponseJson.media_type === "image"){
 
-              } else {
+                document.querySelector("img").src = apodResponseJson.hdurl
 
-              let iframe=document.querySelector("iframe");
-              iframe.src = apodResponseJson.url
-              iframe.style.display="block"
-            }
+                } else {
 
-            document.getElementById("description").innerText= apodResponseJson.explanation;
+                let iframe = document.querySelector("iframe");
+                iframe.src = apodResponseJson.url
+                iframe.style.display = "block"
+              }
+
+              document.getElementById("description").innerText= apodResponseJson.explanation;
+            })
+            .catch(err => {
+              console.log(`error ${err}`)
+              alert("Sorry, there are no results for your search")
           });
-    }
-  )
+      }
+    )
+  })
 }
 
 window.addEventListener("load", loadEvent) 
